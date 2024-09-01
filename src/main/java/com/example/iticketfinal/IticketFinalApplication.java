ca21799ef0c96538d474386e0cf1a4eabb57ed52
Baloghlan Gasimov <qasimovbaloglan13@gmail.com>
2024-09-01 15:02:18 +0400

    Add files via upload

---
+++ src/main/java/com/example/iticketfinal/IticketFinalApplication.java
@@ -0,0 +1,13 @@
+package com.example.iticketfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IticketFinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IticketFinalApplication.class, args);
+    }
+
+}
