d4a80f62bba29fe735f31e294cff93f215d64c5a
Baloghlan Gasimov <baloghlan.gasimov@nikoil.az>
2024-09-09 15:46:28 +0400

    scheduling events expired added

--- src/main/java/com/example/iticketfinal/dao/repository/EventRepository.java
+++ src/main/java/com/example/iticketfinal/dao/repository/EventRepository.java
@@ -3,5 +3,8 @@ package com.example.iticketfinal.dao.repository;
 import com.example.iticketfinal.dao.entity.EventEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EventRepository extends JpaRepository<EventEntity,Long> {
+    List<EventEntity> findAllByExpiredIsFalse();
 }
---
+++ src/main/java/com/example/iticketfinal/schedules/EventExpireDateSchedule.java
@@ -0,0 +1,24 @@
+package com.example.iticketfinal.schedules;
+
+import com.example.iticketfinal.service.EventService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class EventExpireDateSchedule {
+    private final EventService eventService;
+
+    @Scheduled(cron = "0 * * * * *")
+    public void checkEventExpireDateSchedule() {
+        log.info("ActionLog.checkEventExpireDateSchedule.start");
+
+        eventService.checkEventExpire();
+
+        log.info("ActionLog.checkEventExpireDateSchedule.end");
+    }
+
+}
